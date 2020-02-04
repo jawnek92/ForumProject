6b8fcc8a189cc59207ea3cba38add6a8d99e345b
Jakub Wnęk <infil.jw@gmail.com>
2020-02-04 18:53:10 +0100

    Initial commit

---
+++ ForumProject.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ForumProject.Data.Models;
+
+
+
+namespace ForumProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+        public DbSet<Forum> forums { get; set; }
+        public DbSet<Post> posts { get; set; }
+        public DbSet<PostReply> replies { get; set; }
+    }
+}
---
+++ ForumProject.Data/Models/Forum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumProject.Data.Models
+{
+    public class Forum
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string ImageUrl { get; set; }
+        public virtual IEnumerable<Post> Posts { get; set; }
+    }
+}
